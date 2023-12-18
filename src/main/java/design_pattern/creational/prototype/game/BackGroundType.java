11d6ba92ee5288e1b67433b3edd063d189752c35
Neha Sahu <nehasahu9424@gmail.com>
2023-12-18 06:31:55 +0530

    Added pototype Design pattern

---
+++ src/main/java/design_pattern/creational/prototype/game/BackGroundType.java
@@ -0,0 +1,6 @@
+package design_pattern.creational.prototype.game;
+
+
+public enum BackGroundType {
+	TREE,BUILDING,PLANTS,ROOMS
+}
---
+++ src/main/java/design_pattern/creational/prototype/game/GraphicalObject.java
@@ -0,0 +1,10 @@
+package design_pattern.creational.prototype.game;
+
+
+// Step 1: Create a clonable interface
+@FunctionalInterface
+public interface GraphicalObject {	
+	
+	GraphicalObject cloneObject();
+
+}
