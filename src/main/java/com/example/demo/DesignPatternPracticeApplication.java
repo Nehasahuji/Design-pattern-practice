db492579ac1c158c1d4b618213feb19c04e27758
Neha Sahu <nehasahu9424@gmail.com>
2023-12-18 06:44:22 +0530

    Added all other project files

---
+++ src/main/java/com/example/demo/DesignPatternPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternPracticeApplication.class, args);
+	}
+
+}
